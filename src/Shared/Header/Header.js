a934bb30d00490fb3bf30f7b7164e9c3ec869418
Hridoy <rakibhasanhridoy776@gmail.com>
2022-05-08 23:00:18 +0600

    header correction

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -4,6 +4,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, NavLink } from "react-router-dom";
 import auth from "../../firebase.init";
 import { MenuIcon, } from '@heroicons/react/solid'
+import './Header.css'
 const Header = () => {
   const [user] = useAuthState(auth);
   const [navbarOpen, setNavbarOpen]=useState(false)
@@ -33,7 +34,7 @@ const Header = () => {
         <div className="container lg:px-32 px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <Link
-              className="text-2xl font-medium leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-[#b504b5] uppercase "
+              className={colorChange ? "text-2xl font-medium leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-[#b504b5] uppercase title " : "text-2xl font-medium leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-white uppercase title "}
               to="/"
             >
               pristine perfumes
@@ -43,7 +44,7 @@ const Header = () => {
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
             >
-               <MenuIcon className="h-5 w-5 text-[#b504b5]"/> 
+               <MenuIcon className={colorChange ? "h-5 w-5 text-[#b504b5]":"h-5 w-5 text-white"}/> 
             </button>
           </div>
           <div
